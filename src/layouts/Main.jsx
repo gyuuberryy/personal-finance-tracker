08720d2cb4f25b7751cd8d3c6d8d7a71d553f82b
Aadya Sharma <aadya.sharma1700@gmail.com>
2024-10-05 15:18:00 +0800

    other pages

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,26 @@
+// rrd imports
+import { Outlet, useLoaderData } from "react-router-dom";
+
+//  helper functions
+import { fetchData } from "../helpers"
+
+// loader
+export function mainLoader() {
+  const userName = fetchData("userName");
+  return { userName }
+}
+
+const Main = () => {
+  const { userName } = useLoaderData()
+
+  return (
+    <div className="layout">
+      <Nav userName={userName} />
+      <main>
+        <Outlet />
+      </main>
+      <img src={wave} alt="" />
+    </div>
+  )
+}
+export default Main
