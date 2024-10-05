06a8534b7f13e531cb684c2a70932bfcc235dbc1
Aadya Sharma <aadya.sharma1700@gmail.com>
2024-10-05 12:57:44 +0800

    specifications to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,24 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAHExHfMDRu2lVecgYxgW86TXDF-dvsWrY",
-  authDomain: "personal-finance-tracker-ec56e.firebaseapp.com",
-  projectId: "personal-finance-tracker-ec56e",
-  storageBucket: "personal-finance-tracker-ec56e.appspot.com",
-  messagingSenderId: "669303047696",
-  appId: "1:669303047696:web:5b4b41cc896752c03e6c95",
-  measurementId: "G-7X36E69HYW",
+  apiKey: "AIzaSyD2JsoQxdhtz8SeQFVvA2O5jD5c3o4_WOM",
+  authDomain: "savvyspend-e61a9.firebaseapp.com",
+  projectId: "savvyspend-e61a9",
+  storageBucket: "savvyspend-e61a9.appspot.com",
+  messagingSenderId: "85306886161",
+  appId: "1:85306886161:web:e8eeeb87304327064c89b0",
+  measurementId: "G-3YEKWCRQF3"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
